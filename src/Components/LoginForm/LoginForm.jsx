bd26c10211006772a9ddd0816cd085c8fa8f4148
Lucas Gabriel <gabif.johann@gmail.com>
2022-12-10 00:20:10 -0300

    organizacao dos componentes em pastas

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -1,9 +1,9 @@
-import styles from "./Form.module.css";
+import styles from './LoginForm.module.css';
 
 const LoginForm = () => {
   const handleSubmit = (e) => {
     //Nesse handlesubmit você deverá usar o preventDefault,
-    //enviar os dados do formulário e enviá-los no corpo da requisição 
+    //enviar os dados do formulário e enviá-los no corpo da requisição
     //para a rota da api que faz o login /auth
     //lembre-se que essa rota vai retornar um Bearer Token e o mesmo deve ser salvo
     //no localstorage para ser usado em chamadas futuras
@@ -15,25 +15,23 @@ const LoginForm = () => {
     <>
       {/* //Na linha seguinte deverá ser feito um teste se a aplicação
         // está em dark mode e deverá utilizar o css correto */}
-      <div
-        className={`text-center card container ${styles.card}`}
-      >
+      <div className={`text-center card container ${styles.card}`}>
         <div className={`card-body ${styles.CardBody}`}>
           <form onSubmit={handleSubmit}>
             <input
               className={`form-control ${styles.inputSpacing}`}
-              placeholder="Login"
-              name="login"
+              placeholder='Login'
+              name='login'
               required
             />
             <input
               className={`form-control ${styles.inputSpacing}`}
-              placeholder="Password"
-              name="password"
-              type="password"
+              placeholder='Password'
+              name='password'
+              type='password'
               required
             />
-            <button className="btn btn-primary" type="submit">
+            <button className='btn btn-primary' type='submit'>
               Send
             </button>
           </form>
