5c95474680e47c24bff1380169cf59efd0c4f399
Lucas Gabriel <gabif.johann@gmail.com>
2022-12-10 23:42:35 -0300

    add URL de base

--- src/functions/authUser.js
+++ src/functions/authUser.js
@@ -1,26 +1,23 @@
 import axios from 'axios';
-
-const baseUrl = "https://dhodonto.ctdprojetos.com.br";
+import { api } from '../services/api';
 
 export const authUser = async (loginData) => {
-    try{
-        const data = await axios.post(`${baseUrl}/auth`, { ...loginData });
-        return ({...data.data, logged: true});
-    }
-    catch{
-        return({logged: false});
-    }
-}
-
-export const isUserLoggedIn = () => {
-    if(localStorage.getItem('loginData')){
-        return JSON.parse(localStorage.getItem('loginData'));
-    }
-    else{
-        return false;
-    }
+  try {
+    const data = await axios.post(`${api}/auth`, { ...loginData });
+    return { ...data.data, logged: true };
+  } catch {
+    return { logged: false };
+  }
 };
 
-export const userLogout = () =>{
-    localStorage.clear();
-}
+export const isUserLoggedIn = () => {
+  if (localStorage.getItem('loginData')) {
+    return JSON.parse(localStorage.getItem('loginData'));
+  } else {
+    return false;
+  }
+};
+
+export const userLogout = () => {
+  localStorage.clear();
+};
