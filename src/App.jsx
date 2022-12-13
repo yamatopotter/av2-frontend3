70c58879c46fcf919e361489be3d5f7aa0936b7d
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2022-12-13 11:48:50 -0300

    fix: pequenos ajustes e correção de códigos e exportações

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,11 @@ import { useState, useEffect } from 'react';
 import { Outlet, useNavigate, useLocation } from 'react-router-dom';
 import { Footer } from './Components/Footer/Footer';
 import { Navbar } from './Components/Navbar/Navbar';
-
 import { ThemeColor } from './functions/context';
 import { isUserLoggedIn } from './functions/authUser';
 
 export function App() {
-  const [color, setColor] = useState('light');
-
-  /*Na linha seguinte deverá ser feito um teste se a aplicação
-        está em dark mode e deverá utilizar a classe dark ou light */
-
-function App() {
+  
   const [color, setColor] = useState(localStorage.getItem('color'));
   const isUserLogged = isUserLoggedIn();
   const navigate = useNavigate();
@@ -26,7 +20,7 @@ function App() {
     else if(!isUserLogged && location.pathname === '/'){
       navigate("/login");
     }
-  }, [isUserLogged])
+  }, [isUserLogged, location, navigate])
 
   return (
       <ThemeColor.Provider value={[color, setColor]}>
