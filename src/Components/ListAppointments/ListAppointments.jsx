8f20d48a96d8b28cb5e90622c2aab2c84d1a01a2
Lucas Gabriel <gabif.johann@gmail.com>
2022-12-14 20:44:52 -0300

    identacao

--- src/Components/ListAppointments/ListAppointments.jsx
+++ src/Components/ListAppointments/ListAppointments.jsx
@@ -1,50 +1,70 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { getAllAppointments } from "../../functions/api";
-import styles from "./ListAppointments.module.css";
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getAllAppointments } from '../../functions/api';
+import styles from './ListAppointments.module.css';
 
 export const ListAppointments = () => {
-    const [tableData, setTableData] = useState([]);
-    const monthNames = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"];
+  const [tableData, setTableData] = useState([]);
+  const monthNames = [
+    'Jan',
+    'Fev',
+    'Mar',
+    'Abr',
+    'Mai',
+    'Jun',
+    'Jul',
+    'Ago',
+    'Set',
+    'Out',
+    'Nov',
+    'Dez',
+  ];
 
-    useEffect(() => {
-        async function getData(){
-            const data = await getAllAppointments();
-            setTableData(data);
-            return data;
-        }
-        
-        getData();
-    }, [])
+  useEffect(() => {
+    async function getData() {
+      const data = await getAllAppointments();
+      setTableData(data);
+      return data;
+    }
 
-    return (
-        <div className={`row`}>
-            {
-                tableData.map((data, index)=>{
-                const date = new Date(data.dataHoraAgendamento);
+    getData();
+  }, []);
 
-                return (
-                <div className="col-12 col-md-6 col-lg-3" key={index}>
-                    <div className={ `card ${styles.cardAppointment}` } >
-                        <div>
-                            <span className={styles.month}>{monthNames[date.getMonth()]}</span>
-                            <Link className={styles.deleteButton} to={`/consulta/${data.dentista.matricula}/${data.dataHoraAgendamento}`}>🗑</Link>
-                            <span className={styles.day}>{date.getDay()}</span>
-                        </div>
-                        <div className={styles.nomeHolder}>
-                            <p className={styles.nomePaciente}>
-                                <span className={styles.whoIs}>paciente</span>
-                                {data.paciente.nome} {data.paciente.sobrenome}
-                            </p>
-                            <span className={styles.verticalLine} />
-                            <p className={styles.nomeDentista}>
-                                <span className={styles.whoIs}>dentista</span>
-                                {data.dentista.nome} {data.dentista.sobrenome}
-                            </p>
-                        </div>
-                    </div>
-                </div>)})
-            }
-        </div>
-    );
-}
+  return (
+    <div className={`row`}>
+      {tableData.map((data, index) => {
+        const date = new Date(data.dataHoraAgendamento);
+
+        return (
+          <div className='col-12 col-md-6 col-lg-3' key={index}>
+            <div className={`card ${styles.cardAppointment}`}>
+              <div>
+                <span className={styles.month}>
+                  {monthNames[date.getMonth()]}
+                </span>
+                <Link
+                  className={styles.deleteButton}
+                  to={`/consulta/${data.dentista.matricula}/${data.dataHoraAgendamento}`}
+                >
+                  🗑
+                </Link>
+                <span className={styles.day}>{date.getDay()}</span>
+              </div>
+              <div className={styles.nomeHolder}>
+                <p className={styles.nomePaciente}>
+                  <span className={styles.whoIs}>paciente</span>
+                  {data.paciente.nome} {data.paciente.sobrenome}
+                </p>
+                <span className={styles.verticalLine} />
+                <p className={styles.nomeDentista}>
+                  <span className={styles.whoIs}>dentista</span>
+                  {data.dentista.nome} {data.dentista.sobrenome}
+                </p>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,32 +1,43 @@
-import styles from "./Navbar.module.css";
+import styles from './Navbar.module.css';
 import ThemeButton from './ThemeButton/ThemeButton';
 import { Link } from 'react-router-dom';
 
-const LinkLoggedButton = ({color}) => {
-  if (localStorage.getItem('loginData')){
-    if(color === 'dark'){
-      return (<Link className={`nav-link btn btn-light text-${color}`} to="logout">Logout</Link>);
+const LinkLoggedButton = ({ color }) => {
+  if (localStorage.getItem('loginData')) {
+    if (color === 'dark') {
+      return (
+        <Link className={`nav-link btn btn-light text-${color}`} to='logout'>
+          Logout
+        </Link>
+      );
+    } else {
+      return (
+        <Link className={`nav-link btn btn-dark text-${color}`} to='logout'>
+          Logout
+        </Link>
+      );
     }
-    else{
-      return (<Link className={`nav-link btn btn-dark text-${color}`} to="logout">Logout</Link>);
-    }
+  } else {
+    return (
+      <Link className='nav-link' to='login'>
+        Login
+      </Link>
+    );
   }
-  else{
-    return (<Link className="nav-link" to="login">Login</Link>);
-  }
-}
+};
 
-
-export const Navbar = ({color}) => {  
+export const Navbar = ({ color }) => {
   return (
-    <header className="sticky-top">
+    <header className='sticky-top'>
       <nav
         className={`navbar navbar-expand-sm navbar-${color} bg-${color}`}
         aria-label='Third navbar example'
       >
         <div className='container'>
           {/* Ao clicar, o usuário deve ser redirecionado a home, com react-router */}
-          <Link className={`navbar-brand ${styles.navbarBrand}`} to="home">DH Odonto</Link>
+          <Link className={`navbar-brand ${styles.navbarBrand}`} to='home'>
+            DH Odonto
+          </Link>
           <button
             className='navbar-toggler'
             type='button'
@@ -45,30 +56,74 @@ export const Navbar = ({color}) => {
           >
             <ul className='navbar-nav mb-2 mb-sm-0'>
               <li className={`nav-item ${styles.navBarLink}`}>
-                <Link className="nav-link" to="home">Home</Link>
+                <Link className='nav-link' to='home'>
+                  Home
+                </Link>
               </li>
 
               <li className={`nav-item ${styles.navBarLink} dropdown`}>
-                <Link className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Administração</Link>
-                  <ul className="dropdown-menu">
-                    <li><Link className="dropdown-item" to="dentistas">Administrar dentistas</Link></li>
-                    <li><Link className="dropdown-item" to="#">Adicionar dentista</Link></li>
-                    <li><hr className="dropdown-divider" /></li>
-                    <li><Link className="dropdown-item" to="pacientes">Administrar pacientes</Link></li>
-                    <li><Link className="dropdown-item" to="#">Adicionar paciente</Link></li>
-                  </ul>
+                <Link
+                  className='nav-link dropdown-toggle'
+                  to='#'
+                  role='button'
+                  data-bs-toggle='dropdown'
+                  aria-expanded='false'
+                >
+                  Administração
+                </Link>
+                <ul className='dropdown-menu'>
+                  <li>
+                    <Link className='dropdown-item' to='dentistas'>
+                      Administrar dentistas
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className='dropdown-item' to='#'>
+                      Adicionar dentista
+                    </Link>
+                  </li>
+                  <li>
+                    <hr className='dropdown-divider' />
+                  </li>
+                  <li>
+                    <Link className='dropdown-item' to='pacientes'>
+                      Administrar pacientes
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className='dropdown-item' to='#'>
+                      Adicionar paciente
+                    </Link>
+                  </li>
+                </ul>
               </li>
 
               <li className={`nav-item ${styles.navBarLink} dropdown`}>
-                <Link className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Consultas</Link>
-                  <ul className="dropdown-menu">
-                    <li><Link className="dropdown-item" to="consultas">Administrar Consutlas</Link></li>
-                    <li><Link className="dropdown-item" to="#">Agendar Consuta</Link></li>
-                  </ul>
+                <Link
+                  className='nav-link dropdown-toggle'
+                  to='#'
+                  role='button'
+                  data-bs-toggle='dropdown'
+                  aria-expanded='false'
+                >
+                  Consultas
+                </Link>
+                <ul className='dropdown-menu'>
+                  <li>
+                    <Link className='dropdown-item' to='consultas'>
+                      Administrar Consultas
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className='dropdown-item' to='#'>
+                      Agendar Consulta
+                    </Link>
+                  </li>
+                </ul>
               </li>
 
-              <li className={`nav-item ${styles.navBarLink}`}>                
-                <LinkLoggedButton color={color}/>
+              <li className={`nav-item ${styles.navBarLink}`}>
+                <LinkLoggedButton color={color} />
               </li>
               <ThemeButton styles={styles.btnStyle} />
             </ul>
@@ -77,4 +132,4 @@ export const Navbar = ({color}) => {
       </nav>
     </header>
   );
-}
+};
